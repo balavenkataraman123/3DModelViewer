9026a5b835a3991921225a0d7ef1c24242ce3db4
Ioannis Iliadis <giannisiliadis2k15@gmail.com>
2024-06-30 16:23:16 +1000

    created vba class

---
+++ src/opengl/vertex_array.cpp
@@ -0,0 +1,82 @@
+//
+// Created by Gianni on 30/06/2024.
+//
+
+#include "vertex_array.hpp"
+
+
+VertexArray::VertexArray()
+    : m_renderer_id()
+{
+    glCreateVertexArrays(1, &m_renderer_id);
+}
+
+VertexArray::~VertexArray()
+{
+    if (m_renderer_id)
+    {
+        glDeleteVertexArrays(1, &m_renderer_id);
+    }
+}
+
+VertexArray::VertexArray(VertexArray &&other) noexcept
+{
+    m_renderer_id = other.m_renderer_id;
+    other.m_renderer_id = 0;
+}
+
+VertexArray &VertexArray::operator=(VertexArray &&other) noexcept
+{
+    if (this != &other)
+    {
+        if (m_renderer_id)
+        {
+            glDeleteVertexArrays(1, &m_renderer_id);
+        }
+
+        m_renderer_id = other.m_renderer_id;
+        other.m_renderer_id = 0;
+    }
+
+    return *this;
+}
+
+void VertexArray::bind() const
+{
+    glBindVertexArray(m_renderer_id);
+}
+
+void VertexArray::unbind() const
+{
+    glBindVertexArray(0);
+}
+
+void VertexArray::attach_vertex_buffer(const VertexBuffer &vbo, const VertexBufferLayout &layout)
+{
+    bind();
+    vbo.bind();
+
+    uint32_t offset = 0;
+    uint32_t stride = layout.stride();
+
+    for (const auto& attrib : layout.attributes())
+    {
+        glEnableVertexAttribArray(attrib.index);
+        glVertexAttribPointer(attrib.index, attrib.size, attrib.type, attrib.normalize, stride, reinterpret_cast<void*>(offset));
+        offset += attrib.size * utils::gl_sizeof(attrib.type);
+    }
+}
+
+void VertexArray::attach_index_buffer(const IndexBuffer &ibo)
+{
+    bind();
+    ibo.bind();
+
+    unbind();
+    ibo.unbind();
+}
+
+uint32_t VertexArray::id() const
+{
+    return m_renderer_id;
+}
---
+++ src/opengl/vertex_array.hpp
@@ -0,0 +1,38 @@
+//
+// Created by Gianni on 30/06/2024.
+//
+
+#ifndef INC_3DMODELVIEWER_VERTEX_ARRAY_HPP
+#define INC_3DMODELVIEWER_VERTEX_ARRAY_HPP
+
+#include "vertex_buffer.hpp"
+#include "index_buffer.hpp"
+#include "vertex_buffer_layout.hpp"
+
+
+class VertexArray
+{
+public:
+    VertexArray();
+    ~VertexArray();
+
+    VertexArray(VertexArray&& other) noexcept;
+    VertexArray& operator=(VertexArray&& other) noexcept;
+
+    VertexArray(const VertexArray&) = delete;
+    VertexArray& operator=(const VertexArray&) = delete;
+
+    void bind() const;
+    void unbind() const;
+
+    void attach_vertex_buffer(const VertexBuffer& vbo, const VertexBufferLayout& layout);
+    void attach_index_buffer(const IndexBuffer& ibo);
+
+    uint32_t id() const;
+
+private:
+    uint32_t m_renderer_id;
+};
+
+
+#endif //INC_3DMODELVIEWER_VERTEX_ARRAY_HPP
